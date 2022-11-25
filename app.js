8b0aa523f543422fdabfae688cfbd3ff1c1a39c4
Gowri239 <gowrimopuru14@gmail.com>
2022-11-25 20:50:35 +0530

    make it better

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ Downloadurl.belongsTo(User);
 
 sequelize.sync()
   .then(() => {
-    app.listen(process.env.PORT || 3000)
+    app.listen(process.env.PORT || 3000) 
   })
   .catch((err) => {
     console.log(err)
