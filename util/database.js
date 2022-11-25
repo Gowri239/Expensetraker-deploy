3e6a50846600eab5ad8a7216644dd7e5e2c8e936
Gowri239 <gowrimopuru14@gmail.com>
2022-11-25 21:21:17 +0530

    another change

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,  {
   dialect: 'mysql',
   host: process.env.DB_HOST,
-  port: 3306
+  port: "3306"
 });
 
 module.exports = sequelize;
